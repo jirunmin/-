05ad073b0f1dea7e8e15f2a29544e44683873167
JiRunmin <3048390962@qq.com>
2023-09-07 14:57:53 +0800

    货物价值数字显示

--- VendingMachine.v
+++ VendingMachine.v
@@ -11,19 +11,63 @@ module VendingMachine (
     output wire product_dispensed,            // 输出产品发放信号
     output wire [3:0] sales_total,            // 输出销售总额
     output wire [3:0] cleared_sales_total,     // 输出已清零销售总额
-    output wire [6:0] display_segments,       // 输出用于数码管显示的段
+    output wire [13:0] display_segments,       // 输出用于数码管显示的段
     output wire [3:0] initialized_value,       // 输出初始化值
-    output wire dt_zero							// set dotpoint
+    output wire dt_zero,							// set dotpoint
+    output wire [1:0] state,
+    output reg [6:0] seg,
+    output reg [3:0] q,
+    output wire [7:0] total_sales
 );
 
 assign dt_zero = 1'b1;
+//assign state = 2'b00;
+
 wire [3:0] coin_total;                        // 用于记录硬币总额
 wire [3:0] cleared_total;                     // 用于记录已清零销售总额
 wire clk_1Hz;
 
-// clk_1hz clk1(clk,rst_n,clk_1Hz);
+wire [7:0] product_price;
 
-ClockDivider clock_divider (.clk(clk), .sec_pulse()); // 时钟分频器模块
+Product_codetoprice(.clk(clk),.product_code(product_code), .product_price(product_price));
+
+clk_1hz clk1(clk,rst_n,clk_1Hz);
+
+always @(posedge clk_1Hz or negedge rst_n)
+begin
+   if(~rst_n) q <= 4'h0;
+   else
+   begin
+      if(4'h9 == q) q <= 4'h0;
+      else q <= q + 4'h1;
+   end
+end
+
+always @(q)
+         case(q)
+             4'h0: seg = ~7'h3F;
+             4'h1: seg = ~7'h06;     // - a(0)--
+             4'h2: seg = ~7'h5B;     // |      |
+             4'h3: seg = ~7'h4F;     // f      b(1)
+             4'h4: seg = ~7'h66;     // |      |
+             4'h5: seg = ~7'h6D;     // ---g----
+             4'h6: seg = ~7'h7D;     // |      |
+             4'h7: seg = ~7'h07;     // e      c(2)
+             4'h8: seg = ~7'h7F;     // |      |
+             4'h9: seg = ~7'h67;     // ---d----
+             4'ha: seg = ~7'h77;
+             4'hb: seg = ~7'h7C;
+             4'hc: seg = ~7'h39;
+             4'hd: seg = ~7'h5E;
+             4'he: seg = ~7'h79;
+             4'hf: seg = ~7'h71;
+         endcase
+
+
+
+
+
+//ClockDivider clock_divider (.clk(clk), .sec_pulse()); // 时钟分频器模块
 
 CoinDetector coin_detector (.coin_input(coin_code), .coin_code(coin_total)); // 硬币检测器模块
 
@@ -39,14 +83,16 @@ VendingMachineController controller (          // 自动售货机控制器模块
     .coin_insert_button(coin_insert_button),
     .confirm_button(confirm_button),
     .coin_code(coin_code),
-    .product_code(product_code),
+    .product_price(product_price),
     .alarm(alarm),
     .change(change_amount),
-    .product_dispensed(product_dispensed)
+    .product_dispensed(product_dispensed),
+    .state(state),
+    .total_sales(total_sales)
 );
 
 DisplayModule display (                        // 显示模块
-    .value_to_display(product_code),
+    .value_to_display(product_price),
     .display_segments(display_segments)
 );
 
--- VendingMachineController.v
+++ VendingMachineController.v
@@ -3,33 +3,38 @@ module VendingMachineController (
     input wire coin_insert_button,    // 输入硬币插入按钮信号
     input wire confirm_button,        // 输入确认按钮信号
     input wire [3:0] coin_code,       // 输入硬币代码
-    input wire [3:0] product_code,    // 输入产品代码
+    input wire [7:0] product_price,    // 输入产品代码
     output reg alarm,                 // 输出报警信号
     output reg [3:0] change,          // 输出找零信号
-    output reg product_dispensed      // 输出产品发放信号
-    
+    output reg product_dispensed,      // 输出产品发放信号
+    output reg [1:0] state,
+    output reg [7:0] total_sales
 );
 
-reg [3:0] total_sales = 0;            // 记录总销售额
+//reg [3:0] total_sales = 0;            // 记录总销售额
 reg [3:0] coin_total = 0;             // 记录投币总额
-reg [1:0] state = 2'b00;              // 控制状态机状态的寄存器
+//reg [1:0] state = 2'b00;              // 控制状态机状态的寄存器
+//total_sales = 0;
+
 
 always @(posedge clk) begin
     case (state)
         2'b00: begin // 初始状态
             if (coin_insert_button) begin
+				product_dispensed <= 1'b0;
                 state <= 2'b01; // 进入硬币插入状态
                 coin_total <= coin_code; // 记录插入的硬币金额
             end
         end
-        2'b01: begin // 硬币插入状态
+
+                2'b01: begin // 硬币插入状态
             if (coin_insert_button) begin
                 coin_total <= coin_total + coin_code; // 累加硬币金额
             end
             if (confirm_button) begin
-                if (coin_total >= product_code) begin
-                    total_sales <= total_sales + product_code; // 更新总销售额
-                    change <= coin_total - product_code; // 计算找零
+                if (coin_total >= product_price) begin
+                    total_sales <= total_sales + product_price; // 更新总销售额
+                    change <= coin_total - product_price; // 计算找零
                     product_dispensed <= 1'b1; // 发放产品
                     state <= 2'b10; // 切换到成功状态
                 end else begin

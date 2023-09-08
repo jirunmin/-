0379514e19e4e49c65359bcd4b2a6f6612b33999
JiRunmin <3048390962@qq.com>
2023-09-08 10:12:40 +0800

    完成此项目

--- DisplayModule.v
+++ DisplayModule.v
@@ -11,18 +11,6 @@ assign tens = value_to_display % 100 / 10;
 
 reg [6:0] seven_segment_map [0:9]; // 定义七段数码管编码映射数组，从0到9
 
-initial begin
-    seven_segment_map[0] = ~7'h3F;  // 数码管显示0的编码
-    seven_segment_map[1] = ~7'h06;  // 数码管显示1的编码
-    seven_segment_map[2] = ~7'h5B;  // 数码管显示2的编码
-    seven_segment_map[3] = ~7'h4F;  // 数码管显示3的编码
-    seven_segment_map[4] = ~7'h66;  // 数码管显示4的编码
-    seven_segment_map[5] = ~7'h6D;  // 数码管显示5的编码
-    seven_segment_map[6] = ~7'h7D;  // 数码管显示6的编码
-    seven_segment_map[7] = ~7'h07;  // 数码管显示7的编码
-    seven_segment_map[8] = ~7'h7F;  // 数码管显示8的编码
-    seven_segment_map[9] = ~7'h6F;  // 数码管显示9的编码
-end
 
 assign display_segments = {seven_segment_map[tens], seven_segment_map[ones]}; // 将输入值映射到数码管编码并输出
 
